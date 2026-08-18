413bf0d5afbac2354bb5250a836d68b535390062
mahi-it <mahibenpatel1910@gmail.com>
2026-08-18 17:21:02 +0530

    project1 final

--- project1 Fundamental Booster.py
+++ project1 Fundamental Booster.py
@@ -1,22 +1,21 @@
-print("welcome to the Interactive Personal Data Collector !")
+print("welcome to the Interactive Personal Data Collector !\n ")
 
-name=input("\n Please enter your name : ")
+name=input("Please enter your name : ")
 age=int(input("Please enter your age :"))
 height=float(input("Please enter your height in meter :"))
 fav_number=int(input("Please enter your favourite number :"))
 
-print("\n Thank you! Here is the Information we collected")
+print("\nThank you! Here is the Information we collected\n")
 
-print("\n name :", name  , "(type :", type(name),", Memory Address : ", id(name),")") 
+print("name :", name  , "(type :", type(name),", Memory Address : ", id(name),")") 
 print("age :", age  , "(type :", type(age),", Memory Address: ", id(age),")") 
 print("height :", height  , "(type :", type(height),", Memory Address: ", id(height),")") 
-print("fav_number :", fav_number , "(type :", type(fav_number),", Memory Address : ", id(fav_number),")") 
+print("fav_number :", fav_number , "(type :", type(fav_number),", Memory Address : ", id(fav_number),")\n") 
  
-curr_year = int(input("\n enter curr_year : "))
+curr_year = int(input("enter curr_year : "))
 birth = curr_year - age
 
-print("\n Your Birth  year is appoximately : ", birth ,"(based on your age of",age ,")")
+print("\nYour Birth year is appoximately : ", birth ,"(based on your age of",age ,")\n")
 
-
-print("\n Thank you for using the Personal Data Collector , Goodbye ! ")
+print("Thank you for using the Personal Data Collector , Goodbye ! ")
  
